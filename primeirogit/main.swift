c09a9dc8778ceec877304e5f471de7b2af7cd1ff
leokamei <65233606+kameczera@users.noreply.github.com>
2021-03-01 10:10:17 -0300

    Add files via upload

---
+++ primeirogit/main.swift
@@ -0,0 +1,25 @@
+//Primeira Atividade Swift
+// 1-
+//mais longa
+var numeroDeAula:Int = 3
+//mais curta
+var numeroDeAlunos = 3
+// 2- 
+//Constantes
+let ano :String = "2003"
+let modelo :String = "abcd"
+let fabricante :String = "abcdefg"
+//Variáveis
+var kilometragem :Int = 100
+// 3- anotações de tipo declara o tipo da variável,seja ela string,boolean,number(int/double),etc...
+
+// 4-
+var v1 :Int = 1
+var v2 :Double = 2
+var v3  = String(v2)
+print(type(of: v3))
+
+// 5-
+/* type safety é quando você necessita de deixar claro o tipo de elemento que está usando (exemplo: var v1 :Int = 1)
+type inference é quando você não necessita de deixar claro (exemplo var v1 = 1) pois  já fica implicito qual o programador quer utilizar
+*/
