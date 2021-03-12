5d7ffcf2ab258ac18c250af787e12e8e1bcad5f7
leokamei <65233606+kameczera@users.noreply.github.com>
2021-03-12 08:36:23 -0300

    Atividade2swift

---
+++ playground.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+/* 1- "guard" é usado para fornecer retorno antecipado sem exigir o aninhamento do resto da função. 
+Enquanto "if" permite aninhar seu escopo e não requer nada especial dele. Pode retornar ou não.*/
+
+// 2- Não entendi
+
+// 3-
+func teste(blog:String?) {
+    guard let login:String? = "user"
+    guard let senha:String? = "senha"
+
+}
+
+//4- Não entendi
